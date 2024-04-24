7c0eac87b03ce803f6c73f440b824c15758ff897
Artur Danilov <arthurdanilov1990@gmail.com>
2024-04-24 16:40:27 +0200

    Add project files.

---
+++ Mango.Services.ProductAPI2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mango.Services.ProductAPI2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
