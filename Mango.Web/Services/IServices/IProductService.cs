eba2e6a42a841b1e15d604b847b9f67e3cf7209c
Artur Danilov <arthurdanilov1990@gmail.com>
2024-04-26 14:51:49 +0200

    Add and implement IBaseService

---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web..ProductAPI.Models.Dto;
+using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductAsynk<T>();
+        Task<T> GetAllProductByIdAsynk<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
+    }
+}
