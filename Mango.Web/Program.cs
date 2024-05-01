7b04c7dad5734257b9e8ff8b1b59af1d71fb66ec
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-01 18:36:30 +0200

    Add Bootstrap

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,7 +1,12 @@
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
