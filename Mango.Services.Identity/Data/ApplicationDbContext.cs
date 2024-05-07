7c3d9aa79945cf2ab0835fe6716e2855d037a60f
Artur Danilov <arthurdanilov1990@gmail.com>
2024-05-07 17:51:45 +0200

    Add Identity Server, API Scopes and Clients; Start IS

---
+++ Mango.Services.Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.Identity.DbCotexts
+{
+
+    namespace Mango.Services.ProductAPI.Data
+    {
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+        }
+    }
+}
--- Mango.Services.ProductAPI2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mango.Services.ProductAPI2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
